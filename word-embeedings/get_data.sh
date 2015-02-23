84d2352dd38b31e5d988f27770fdd3b4b68d2b0b
oxinabox <oxinabox@ucc.asn.au>
2015-02-23 09:13:50 +0800

    =

--- word-embeedings/get_data.sh
+++ word-embeedings/get_data.sh
@@ -3,3 +3,10 @@
 
 wget http://metaoptimize.s3.amazonaws.com/cw-embeddings-ACL2010/embeddings-scaled.EMBEDDING_SIZE=50.txt.gz
 gunzip embeddings-scaled.EMBEDDING_SIZE=50.txt.gz
+
+wget http://metaoptimize.s3.amazonaws.com/cw-embeddings-ACL2010/embeddings-scaled.EMBEDDING_SIZE=200.txt.gz
+gunzip embeddings-scaled.EMBEDDING_SIZE=200.txt.gz
+
+wget http://nlp.stanford.edu/~socherr/ACL2012_wordVectorsTextFile.zip
+unzip ACL2012_wordVectorsTextFile.zip
+paste vocab.txt wordVectors.txt > ACL2012_wordvectors.txt
